f7424d094e9fc4dee549ab44ff975eab1ede2b45
BorisLechev <b.k.lechev@gmail.com>
2020-07-22 22:08:58 +0300

    Added _HomePartialView and _TestimonialsPartialView

---
+++ src/Web/MelegPerfumes.Web/wwwroot/gulpFile.js
@@ -0,0 +1,12 @@
+﻿var gulp = require('gulp'),
+    sass = require('grunt-sass');
+
+gulp.task('build-css', function () {
+    return gulp
+        .src('./scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'));
+});
+
+// Default Task
+gulp.task('default', ['build-css']);
