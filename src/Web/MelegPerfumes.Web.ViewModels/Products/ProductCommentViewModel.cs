d90b1e534a4c63338e1984ac1a364ba081e8ce0f
BorisLechev <b.k.lechev@gmail.com>
2020-11-07 17:23:44 +0200

    Fix bug related to the colouring of the heart after up vote.

--- src/Web/MelegPerfumes.Web.ViewModels/Products/ProductCommentViewModel.cs
+++ src/Web/MelegPerfumes.Web.ViewModels/Products/ProductCommentViewModel.cs
@@ -1,12 +1,14 @@
 ﻿namespace MelegPerfumes.Web.ViewModels.Products
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
 
     using AutoMapper;
     using Ganss.XSS;
     using MelegPerfumes.Data.Models;
     using MelegPerfumes.Services.Mapping;
+    using MelegPerfumes.Web.ViewModels.Votes;
 
     public class ProductCommentViewModel : IMapFrom<Comment>, IHaveCustomMappings
     {
@@ -24,12 +26,14 @@
 
         public string UserId { get; set; }
 
-        public int VotesCount { get; set; }
+        public int VotesSum { get; set; }
+
+        public IEnumerable<DetailsVotesViewModel> Votes { get; set; }
 
         public void CreateMappings(IProfileExpression configuration)
         {
             configuration.CreateMap<Comment, ProductCommentViewModel>()
-                .ForMember(dest => dest.VotesCount, opt => opt.MapFrom(src => src.Votes.Sum(v => (int)v.Type)));
+                .ForMember(dest => dest.VotesSum, opt => opt.MapFrom(src => src.Votes.Sum(v => (int)v.Type)));
         }
     }
 }
---
+++ src/Web/MelegPerfumes.Web.ViewModels/Votes/DetailsVotesViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MelegPerfumes.Web.ViewModels.Votes
+{
+    using MelegPerfumes.Data.Models;
+    using MelegPerfumes.Services.Mapping;
+
+    public class DetailsVotesViewModel : IMapFrom<Vote>
+    {
+        public string UserId { get; set; }
+
+        public VoteType Type { get; set; }
+    }
+}
