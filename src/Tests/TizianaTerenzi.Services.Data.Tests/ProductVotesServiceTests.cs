d2cee7fc7e36c99b06d3a720d6b1cb9c58944a93
BorisLechev <b.k.lechev@gmail.com>
2021-02-12 16:19:23 +0200

    Add ProductVotes Service Tests, install NuGet package MockQueryable.Moq (Extensions for mocking EFCore operations such ToListAsync, FirstOrDefaultAsync etc.)

---
+++ src/Tests/TizianaTerenzi.Services.Data.Tests/ProductVotesServiceTests.cs
@@ -0,0 +1,98 @@
+﻿namespace TizianaTerenzi.Services.Data.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.EntityFrameworkCore;
+    using MockQueryable.Moq;
+    using Moq;
+    using TizianaTerenzi.Data;
+    using TizianaTerenzi.Data.Common.Repositories;
+    using TizianaTerenzi.Data.Models;
+    using TizianaTerenzi.Data.Repositories;
+    using TizianaTerenzi.Services.Data.Votes;
+    using Xunit;
+
+    public class ProductVotesServiceTests
+    {
+        [Fact]
+        public async Task WhenUserVotes2TimesOnly1VoteShouldBeCounted()
+        {
+            // Arrange
+            var list = new List<ProductVote>();
+            var mockList = list.AsQueryable().BuildMock();
+
+            var mockRepo = new Mock<IDeletableEntityRepository<ProductVote>>();
+            mockRepo.Setup(pv => pv.All())
+                    .Returns(mockList.Object);
+            mockRepo.Setup(pv => pv.AddAsync(It.IsAny<ProductVote>()))
+                    .Callback((ProductVote vote) => list.Add(vote));
+
+            var service = new ProductVotesService(mockRepo.Object);
+
+            // Act
+            await service.VoteAsync(1, "1", 1);
+            await service.VoteAsync(1, "1", 5);
+            await service.VoteAsync(1, "1", 5);
+            await service.VoteAsync(1, "1", 5);
+            await service.VoteAsync(1, "1", 5);
+
+            // Assert
+            Assert.Single(list);
+            Assert.Equal(5, list.First().Value);
+        }
+
+        [Fact]
+        public async Task When2UsersVoteForTheSameProductTheAverageVoteShouldBeCorrect()
+        {
+            // Arrange
+            var list = new List<ProductVote>();
+            var mockList = list.AsQueryable().BuildMock();
+
+            var mockRepo = new Mock<IDeletableEntityRepository<ProductVote>>();
+            mockRepo.Setup(pv => pv.All())
+                    .Returns(mockList.Object);
+            mockRepo.Setup(pv => pv.AllAsNoTracking())
+                    .Returns(mockList.Object);
+            mockRepo.Setup(pv => pv.AddAsync(It.IsAny<ProductVote>()))
+                    .Callback((ProductVote vote) => list.Add(vote));
+
+            var service = new ProductVotesService(mockRepo.Object);
+
+            // Act
+            await service.VoteAsync(1, "1", 5);
+            await service.VoteAsync(1, "2", 1);
+            await service.VoteAsync(1, "1", 4);
+
+            // how many times the method AddAsync is used
+            mockRepo.Verify(x => x.AddAsync(It.IsAny<ProductVote>()), Times.Exactly(2));
+
+            // Assert
+            Assert.Equal(2, list.Count);
+            Assert.Equal(2, await service.GetNumberOfVotersAsync(1));
+            Assert.Equal(2.5, await service.GetAverageVotesAsync(1));
+        }
+
+        [Fact]
+        public async Task VotesMustBeDeletedWhenDeletingTheProduct()
+        {
+            // Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("test");
+            var dbContext = new ApplicationDbContext(optionsBuilder.Options);
+            var service = new ProductVotesService(new EfDeletableEntityRepository<ProductVote>(dbContext));
+
+            // Act
+            await service.VoteAsync(1, "1", 5);
+            await service.VoteAsync(2, "1", 2);
+
+            await service.DeleteProductVotesAsync(1);
+
+            // Assert
+            Assert.Equal(0, await service.GetNumberOfVotersAsync(1));
+            Assert.Equal(1, await service.GetNumberOfVotersAsync(2));
+            Assert.Equal(0, await service.GetNumberOfVotersAsync(3));
+        }
+    }
+}
