4e16223171ddc0c40b75f66b22fcc5acd0e5d662
BorisLechev <b.k.lechev@gmail.com>
2021-05-15 17:54:36 +0300

    Implement the FULL TEXT Catalog with migration.

---
+++ src/Data/TizianaTerenzi.Data/Migrations/20210515120206_AddSearchCatalogQuery.cs
@@ -0,0 +1,22 @@
+﻿namespace TizianaTerenzi.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class AddSearchCatalogQuery : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "CREATE FULLTEXT CATALOG SearchCatalog AS DEFAULT;",
+                suppressTransaction: true);
+
+            migrationBuilder.Sql(
+                sql: "CREATE FULLTEXT INDEX ON [Products]([SearchText]) KEY INDEX PK_Products;",
+                suppressTransaction: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
