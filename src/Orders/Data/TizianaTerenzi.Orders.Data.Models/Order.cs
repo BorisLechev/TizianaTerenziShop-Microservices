f9a6290685acebd01d5f9dc20c1dcac4ef7f99a9
BorisLechev <b.k.lechev@gmail.com>
2024-01-13 22:25:01 +0200

    Added migration in Order microservice - new repeating columns from IdentityUsers table in Order table.

--- src/Orders/Data/TizianaTerenzi.Orders.Data.Models/Order.cs
+++ src/Orders/Data/TizianaTerenzi.Orders.Data.Models/Order.cs
@@ -11,15 +11,27 @@
 
         public string UserId { get; set; }
 
-        public string UserFirstName { get; set; }
+        public string UserFullName { get; set; }
 
-        public string UserLastName { get; set; }
+        public string UserEmail { get; set; }
+
+        public string UserPhoneNumber { get; set; }
+
+        public string UserShippingAddress { get; set; }
+
+        public string UserCountry { get; set; }
+
+        public string UserTown { get; set; }
+
+        public string UserPostalCode { get; set; }
 
         public int StatusId { get; set; }
 
         public virtual OrderStatus Status { get; set; }
 
         public int? DiscountCodeId { get; set; }
+
+        public byte? CartDiscountCodeValue { get; set; }
 
         public virtual ICollection<OrderProduct> Products { get; set; }
     }
---
+++ src/Orders/Data/TizianaTerenzi.Orders.Data/Migrations/20240113130610_Add_Columns_With_User_Data_In_Orders_Table.cs
@@ -0,0 +1,103 @@
+﻿#nullable disable
+
+namespace TizianaTerenzi.Orders.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    /// <inheritdoc />
+    public partial class Add_Columns_With_User_Data_In_Orders_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserLastName",
+                table: "Orders",
+                newName: "UserTown");
+
+            migrationBuilder.RenameColumn(
+                name: "UserFirstName",
+                table: "Orders",
+                newName: "UserShippingAddress");
+
+            migrationBuilder.AddColumn<byte>(
+                name: "CartDiscountCodeValue",
+                table: "Orders",
+                type: "tinyint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserCountry",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserEmail",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserFullName",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserPhoneNumber",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserPostalCode",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CartDiscountCodeValue",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UserCountry",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UserEmail",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UserFullName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UserPhoneNumber",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UserPostalCode",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "UserTown",
+                table: "Orders",
+                newName: "UserLastName");
+
+            migrationBuilder.RenameColumn(
+                name: "UserShippingAddress",
+                table: "Orders",
+                newName: "UserFirstName");
+        }
+    }
+}
