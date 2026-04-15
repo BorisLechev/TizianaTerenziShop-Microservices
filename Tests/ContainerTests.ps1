0de7c91c13c6fe2e0c4f470f296183867cce5643
BorisLechev <b.k.lechev@gmail.com>
2026-04-15 15:05:46 +0300

    Try to fix ContainerTests.ps1 integration tests.

--- Tests/ContainerTests.ps1
+++ Tests/ContainerTests.ps1
@@ -1,18 +1,23 @@
 $count = 0
+$started = $false
+
 do {
     $count++
-    Write-Output "[$env:STAGE_NAME] Starting container [Attempt: $count]"
-    
-    $testStart = Invoke-WebRequest -Uri http://localhost -UseBasicParsing
-    
-    if ($testStart.statuscode -eq '200') {
-        $started = $true
-    } else {
-        Start-Sleep -Seconds 1
+    Write-Output "[$env:STAGE_NAME] Attempt: $count"
+
+    try {
+        $response = Invoke-WebRequest -Uri http://localhost:5001 -UseBasicParsing
+
+        if ($response.StatusCode -eq 200) {
+            $started = $true
+        }
+    } catch {
+        Start-Sleep -Seconds 3
     }
-    
-} until ($started -or ($count -eq 3))
+
+} until ($started -or ($count -eq 20))
 
 if (!$started) {
+    Write-Error "Service did not start"
     exit 1
 }
